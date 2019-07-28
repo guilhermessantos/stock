86a781734edafd74463c7af3292b6a944505079a
Guilherme Souza <zguilhermesouza@gmail.com>
2019-07-28 11:34:49 -0300

    add list mock

---
+++ src/components/list/__mock__/index.js
@@ -0,0 +1,17 @@
+export const mockCompanies = {
+  contribution: 1200,
+  companies: [{
+    symbol: 'CNTO3',
+    currentPrice: 10,
+    currentPercent: 10
+  }, {
+    symbol: 'ITSA4',
+    currentPrice: 10.40,
+    currentPercent: 10
+  }]
+}
+
+export const mockNoCompanies = {
+  contribution: 1200,
+  companies: []
+}
