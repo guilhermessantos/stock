6cdfadadae6a26802306f2b833f7deac910422c7
Guilherme Souza <zguilhermesouza@gmail.com>
2019-07-30 09:17:42 -0300

    included current percent, percent and amout

--- src/components/list/list.js
+++ src/components/list/list.js
@@ -1,28 +1,60 @@
 import { data } from '../data/data'
+import { company } from '../company/company'
 
 const list = {
   init: () => list.companies(data.get()),
   companies: db => db.companies.length ? list.render(db) : list.alert('Not have companies register'),
   alert: (msg, element = document.querySelector('.list')) => element.innerHTML = msg,
+  sumPercent: db => {
+    let total = 0
+
+    db.companies.map(item => {
+      total += item.currentPercent
+    })
+
+    return total
+  },
+  remove: (element, db) => {
+    const buttonRemove = document.querySelectorAll(element)
+
+    Array.from(buttonRemove).forEach(el => {
+      el.addEventListener('click', event => {
+        const symbol = event.target.getAttribute('data-company-name')
+
+        company.remove(db, symbol)
+        list.init()
+      })
+    })
+  },
   render: (db, element = document.querySelector('.list')) => {
-      const companies = db.companies.map(company => {
-      const percent = company.percent ? company.percent : 0
-      const contribution = company.contribution ? company.contribution : 0
-      const amout = company.amount ? company.amount : 0
+    const sumPercent = list.sumPercent(db)
+    const contributionDb = data.get().contribution
+    const amount = []
+
+    const companies = db.companies.map(company => {
+      let currentPrice = company.currentPrice
+      const percent = company.currentPercent / sumPercent * 100
+      const contribution = percent * contributionDb / 100
+
+      for(let i = currentPrice; i <= contribution; i += currentPrice) {
+        amount.push(i)
+      }
 
       return `
         <div class="company">
           <div class="company__symbol">${company.symbol}</div>
-          <div class="company__current-price">R$ ${company.currentPrice}</div>
+          <div class="company__current-price">R$ ${currentPrice}</div>
           <div class="company__current-percent">${company.currentPercent}%</div>
-          <div class="company__percent">${percent}%</div>
-          <div class="company__contribution">R$ ${contribution}</div>
-          <div class="company__amount">${amout}</div>
+          <div class="company__percent">${parseInt(percent)}%</div>
+          <div class="company__contribution">R$ ${parseInt(contribution)}</div>
+          <div class="company__amount">${amount.length}</div>
+          <button class="company__remove" data-company-name="${company.symbol}">X</button>
         </div><hr/>
       `
     }).join('')
 
-    return element.innerHTML = companies
+    element.innerHTML = companies
+    list.remove('.company__remove', db)
   }
 }
 
