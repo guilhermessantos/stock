b16c64f2fc68d61b53569a50853fceb230385032
Guilherme Souza <zguilhermesouza@gmail.com>
2019-12-08 19:03:41 -0300

    dynamic company  search and add participation

--- src/components/search-company/styles.js
+++ src/components/search-company/styles.js
@@ -1,7 +1,17 @@
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
 import { FieldGroup } from '../field-group'
 import { Container as Field } from '../field/styles'
 import { Button } from '../button'
+
+const rotate = keyframes`
+  from {
+    transform: rotate(0)
+  }
+
+  to {
+    transform: rotate(360deg)
+  }
+`
 
 export const Container = styled.div`
   align-items: flex-end;
@@ -9,7 +19,31 @@ export const Container = styled.div`
   margin-bottom: 40px;
 
   ${FieldGroup} {
+    position: relative;
     min-width: 330px;
+
+    > svg {
+      fill: #00214d;
+      position: absolute;
+      left: 15px;
+      bottom: 17px;
+      transition: opacity .2s, transform .3s;
+      opacity: ${props => props.hasCompany ? 1 : 0};
+      transform: translateX(${props => props.hasCompany ? 0 : '-20px'})
+    }
+
+    input {
+      padding-left: ${props => props.hasCompany && '40px'};
+      border-color: ${props => props.hasCompany && '#00ebc7'};
+      transition: padding-left .3s;
+      text-transform: uppercase;
+    }
+
+    button svg {
+      animation: ${rotate} .5s linear infinite;
+      position: relative;
+      top: 2px;
+    }
   }
 
   > ${Field} {
@@ -18,5 +52,23 @@ export const Container = styled.div`
 
   > ${Button} {
     min-width: 150px;
+  }
+`
+
+export const Message = styled.div`
+  align-items: center;
+  color: #ec7669;
+  font-size: 14px;
+  display: flex;
+  position: absolute;
+  top: calc(100% + 5px);
+
+  svg {
+    margin-right: 5px;
+  }
+
+  strong {
+    margin-left: 4px;
+    text-transform: uppercase;
   }
 `
