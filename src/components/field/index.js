e8df6c656cee91d68c120cc0b90d79c967bc6809
Guilherme Souza <zguilhermesouza@gmail.com>
2019-12-08 18:59:33 -0300

    add prop children

--- src/components/field/index.js
+++ src/components/field/index.js
@@ -2,17 +2,20 @@ import React from 'react'
 import t from 'prop-types'
 import { Container } from './styles'
 
-const Field = ({ id, label, placeholder }) => (
-  <Container>
+const Field = ({ id, label, placeholder, onChange, value, disabled, error, refProp, children }) => (
+  <Container error={error}>
     <label htmlFor={id}>{label}</label>
-    <input id={id} type="text" placeholder={placeholder} />
+    {children ? children : <input ref={refProp} id={id} type="text" placeholder={placeholder} onChange={onChange} value={value} disabled={disabled} />}
   </Container>
 )
 
 Field.propTypes = {
   id: t.string,
   label: t.string,
-  placeholder: t.string
+  value: t.string,
+  disabled: t.bool,
+  placeholder: t.string,
+  error: t.bool
 }
 
 export { Field }
--- src/components/field/styles.js
+++ src/components/field/styles.js
@@ -10,16 +10,22 @@ export const Container = styled.div`
   }
 
   input {
-    border: 2px solid #DDD;
+    border-width: 2px;
+    border-style: solid;
+    border-color: ${props => props.error ? '#ec7669' : '#DDD'};
     border-radius: 5px;
     font-size: 16px;
     height: 50px;
     padding: 0 15px;
-    width: 100%;
     transition: border .2s;
+    width: 100%;
 
     &:focus {
       border-color: #00ebc7;
+    }
+
+    &:disabled {
+      background: #f8f8f8;
     }
 
     ::placeholder {
