ce16e7aab56f38dddc776b543e0efea4907444d8
Guilherme Souza <zguilhermesouza@gmail.com>
2019-12-09 20:48:35 -0300

    new build

---
+++ docs/_next/static/webpack/static/development/pages/index.js.96eacaa9151d516419c6.hot-update.js
@@ -0,0 +1,32 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./src/styles/global.js":
+/*!******************************!*\
+  !*** ./src/styles/global.js ***!
+  \******************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _babel_runtime_corejs2_helpers_esm_taggedTemplateLiteral__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @babel/runtime-corejs2/helpers/esm/taggedTemplateLiteral */ "./node_modules/@babel/runtime-corejs2/helpers/esm/taggedTemplateLiteral.js");
+/* harmony import */ var styled_components__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! styled-components */ "./node_modules/styled-components/dist/styled-components.browser.esm.js");
+
+
+function _templateObject() {
+  var data = Object(_babel_runtime_corejs2_helpers_esm_taggedTemplateLiteral__WEBPACK_IMPORTED_MODULE_0__["default"])(["\n  @import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700&display=swap');\n\n  * {\n    box-sizing: border-box;\n    margin: 0;\n    outline: 0;\n    padding: 0;\n  }\n\n  html,\n  body,\n  #root {\n    height: 100%;\n  }\n\n  body {\n    position: relative;\n\n    @media (min-width: 480px) {\n      padding-bottom: 40px;\n    }\n  }\n\n  body,\n  input,\n  button {\n    appearance: none;\n    font-family: 'Source Sans Pro', sans-serif;\n  }\n\n  #__next {\n    max-width: 660px;\n    padding-left: 15px;\n    padding-right: 15px;\n    margin-left: auto;\n    margin-right: auto;\n  }\n\n  .link_github {\n    font-size: 14px;\n    align-items: center;\n    bottom: 10px;\n    color: #24292e;\n    display: inline-flex;\n    left: 50%;\n    position: absolute;\n    text-decoration: none;\n    transform: translateX(-50%);\n\n    svg {\n      fill: currentColor;\n      margin-right: 3px;\n    }\n  }\n"]);
+
+  _templateObject = function _templateObject() {
+    return data;
+  };
+
+  return data;
+}
+
+
+/* harmony default export */ __webpack_exports__["default"] = (Object(styled_components__WEBPACK_IMPORTED_MODULE_1__["createGlobalStyle"])(_templateObject()));
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.96eacaa9151d516419c6.hot-update.js.map
