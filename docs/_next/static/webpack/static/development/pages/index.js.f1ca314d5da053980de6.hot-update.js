4300c2139b0746bbcad782cd575bbcf968f833fa
Guilherme Souza <zguilhermesouza@gmail.com>
2019-12-10 12:53:27 -0300

    new build

---
+++ docs/_next/static/webpack/static/development/pages/index.js.f1ca314d5da053980de6.hot-update.js
@@ -0,0 +1,43 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./src/styles/global.js":
+/*!******************************!*\
+  !*** ./src/styles/global.js ***!
+  \******************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _babel_runtime_corejs2_helpers_esm_taggedTemplateLiteral__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @babel/runtime-corejs2/helpers/esm/taggedTemplateLiteral */ "./node_modules/@babel/runtime-corejs2/helpers/esm/taggedTemplateLiteral.js");
+/* harmony import */ var styled_components__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! styled-components */ "./node_modules/styled-components/dist/styled-components.browser.esm.js");
+
+
+function _templateObject2() {
+  var data = Object(_babel_runtime_corejs2_helpers_esm_taggedTemplateLiteral__WEBPACK_IMPORTED_MODULE_0__["default"])(["\n  @import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700&display=swap');\n\n  * {\n    box-sizing: border-box;\n    margin: 0;\n    outline: 0;\n    padding: 0;\n  }\n\n  html,\n  body,\n  #root {\n    height: 100%;\n  }\n\n  body {\n    padding-top: 15px;\n\n    @media (min-width: 480px) {\n      padding-bottom: 40px;\n      padding-top: 40px;\n    }\n  }\n\n  body,\n  input,\n  button {\n    appearance: none;\n    font-family: 'Source Sans Pro', sans-serif;\n  }\n\n  #__next {\n    max-width: 660px;\n    padding-left: 15px;\n    padding-right: 15px;\n    margin-left: auto;\n    margin-right: auto;\n  }\n\n  .link_github {\n    top: 0;\n    position: absolute;\n    right: 0;\n    text-decoration: none;\n\n    &::before {\n      border-color: #DDD #DDD transparent transparent;\n      border-style: solid;\n      border-width: 28px;\n      content: \"\";\n      position: absolute;\n      top: 0;\n      right: 0;\n    }\n\n    svg {\n      fill: #00214d;\n      position: relative;\n      top: 10px;\n      right: 6px;\n    }\n  }\n\n  .load {\n    align-items: center;\n    display: flex;\n    background: rgba(255, 255, 255, .9);\n    bottom: 0;\n    justify-content: center;\n    left: 0;\n    position: fixed;\n    right: 0;\n    top: 0;\n\n    svg {\n      animation: ", " .5s linear infinite;\n    }\n  }\n"]);
+
+  _templateObject2 = function _templateObject2() {
+    return data;
+  };
+
+  return data;
+}
+
+function _templateObject() {
+  var data = Object(_babel_runtime_corejs2_helpers_esm_taggedTemplateLiteral__WEBPACK_IMPORTED_MODULE_0__["default"])(["\n  from {\n    transform: rotate(0)\n  }\n\n  to {\n    transform: rotate(360deg)\n  }\n"]);
+
+  _templateObject = function _templateObject() {
+    return data;
+  };
+
+  return data;
+}
+
+
+var rotate = Object(styled_components__WEBPACK_IMPORTED_MODULE_1__["keyframes"])(_templateObject());
+/* harmony default export */ __webpack_exports__["default"] = (Object(styled_components__WEBPACK_IMPORTED_MODULE_1__["createGlobalStyle"])(_templateObject2(), rotate));
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.f1ca314d5da053980de6.hot-update.js.map
